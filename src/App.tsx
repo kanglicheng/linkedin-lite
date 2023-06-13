11b301236a0b51c1143507589e6b47b692d7baeb
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2023-06-13 15:27:17 -0400

    ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Box, Button, ChakraProvider, Checkbox, Container, FormControl, FormLabel, Grid, Heading, Icon, Input, Link, ListItem, OrderedList, Text, VStack } from '@chakra-ui/react';
 import React from "react";
 
-import { Box, Button, ChakraProvider, Checkbox, Container, FormControl, FormLabel, Grid, Heading, Input, Link, Text, VStack } from '@chakra-ui/react';
+import { createClient } from '@supabase/supabase-js';
+import { FaLinkedin } from "react-icons/fa";
 import "./App.css";
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 function App() {
 
@@ -11,6 +17,7 @@ function App() {
   const [linkedin, setLinkedin] = React.useState('');
 
   const [checkedItems, setCheckedItems] = React.useState(new Set());
+
 
   const handleCheck = (e) => {
     if(e.target.checked){
@@ -31,6 +38,30 @@ function App() {
     margin: "10px"
   }
 
+  const isDisabled = React.useMemo(()=>{
+    if(!email || !first || !last){
+      return true;
+    }
+    return false;
+  }, [email, first, last])
+
+  const insertLead = async () => {
+
+    const roles = Array.from(checkedItems).join(';')
+    const { data, error } = await supabase
+    .from('Leads')
+    .insert([
+      { first: first, last: last, email: email, linkedin: linkedin, roles: roles },
+    ])
+    if(!error){
+      setFirst('')
+      setLast('')
+      setEmail('')
+      setLinkedin('')
+      setCheckedItems(new Set());
+    }
+  }
+
   return (
     <ChakraProvider>
 
@@ -49,26 +80,57 @@ function App() {
           get the best job that they can. We offer mock interviews, resume review, and general career coaching. Presently 
           we work mainly with new graduates and professionals with less than ~3 years of experience.
         </Text>
-        <Heading as='h4' size="md">
+
+        <Heading as='h4' size="sm" style={{marginTop: "5px"}}>
+          What We Offer
+        </Heading>
+        <Text>
+          We have noticed that one of the biggest differentiator in job 
+          search success is having access to <em>quality mock interviews.</em> We offer mock interviews that closely simulate the actual process and we take time 
+          to give detailed and actionable feedback, including but not limited to customized study plans and suggested reading. We are also always happy to provide informal mentorship.
+        </Text>
+        <Heading as='h4' size="sm" style={{marginTop: "5px"}}>
+          Our Process
+        </Heading>
+        <Text>
+        </Text>
+        <OrderedList>
+          <ListItem>Fill out an application below</ListItem>
+          <ListItem>Set up a 30 minute phone call to discuss background and career aspirations</ListItem>
+          <ListItem>Intense and realistic interview preparation via a series of mock interviews, tailored according to the role(s) you 
+            are applying and interviewing for
+          </ListItem>
+          <ListItem> Debrief with mock interviewer. Receive detailed feedback and specific recommendations for improvement</ListItem>
+          <ListItem> Stay in touch. We are not here to just offer a set of mock interviews. We are here to aid in the job search process and will
+            be available for further support.
+          </ListItem>
+        </OrderedList>
+        <Heading as='h4' size="md" style={{marginTop: "10px"}}>
           About the Interviewers
         </Heading>
         <VStack spacing="2em" sx={{marginTop: "10px"}}>
           <Text>
-            <em>Anish Shah </em>is currently a quantitative trader at <Link color='teal.500' href="https://www.mlp.com/">Millennium </Link> in Manhattan. 
+            <em><b>Anish Shah</b> </em> <Link href={"https://www.linkedin.com/in/anishshah500/"}><Icon as={FaLinkedin}/></Link> is currently a quantitative trader at <Link color='teal.500' href="https://www.mlp.com/">Millennium </Link> in Manhattan. 
             Previously he worked in quantitative research at Goldman Sachs and earned a Masters degree in quantitative finance (MSCF) from Carnegie Mellon 
             University. Anish has interviewed over one hundred candidates for the Strats role while employed at Goldman. He has also interviewed with over forty
-            buy-side firms and top investment banks and as a result has deep and extensive knowledge of interviews in the financial industry.
+            buy-side firms and top investment banks and as a result has deep and extensive knowledge of interviews in the financial industry. 
+          
           </Text>
           <Text>
-            <em>Stephen Cheng</em> is currently a Vice President of Technology at <Link color='teal.500' href="https://www.jpmorganchase.com/">JPMorgan and Chase.</Link> 
-            In the past, Stephen has worked at Uber and Microsoft as a software engineer. Stephen has also mentored three individuals who are now all employed at top technology companies.
+            <em><b>Stephen Cheng</b></em> <Link href={"https://www.linkedin.com/in/kanglicheng/"}><Icon as={FaLinkedin}/></Link> is currently a Vice President of Technology at <Link color='teal.500' href="https://www.jpmorganchase.com/">JPMorgan and Chase. </Link> 
+            In the past, Stephen has worked at Uber and Microsoft as a software engineer. Stephen has also mentored over a dozen individuals over the course of 6 months to a year 
+            and his mentees are now all employed at top technology companies.
             He has interviewed with over 50 top technology companies, hedge funds, and investment banks for the role of software engineer.
           </Text>
-
+          <Text>
+            <em><b>Peter Scagnelli</b></em> is current a machine learning engineer at <Link color='teal.500' href="https://aws.amazon.com/">AWS Amazon. </Link> He has mentored over twenty individuals to help them
+            obtain roles at top companies, including Google, Meta, Citadel, and IMC. He is a skilled negotiator and gives the toughest but also most helpful 
+            mock behavioral interviews.
+          </Text>
         </VStack>
       </Container>
       <Container>
-      <Heading as='h4' size="md">
+      <Heading as='h4' size="md" style={{marginTop: "10px"}}>
         Apply to work with us
       </Heading>
         <FormControl isRequired>
@@ -83,7 +145,7 @@ function App() {
         </FormControl>
         <VStack spacing='10px'>
             <FormLabel>Linkedin Profile</FormLabel>
-            <Input value={linkedin} type="url" onChange={(e)=>setEmail(e.target.value)}/>
+            <Input value={linkedin} type="url" onChange={(e)=>setLinkedin(e.target.value)}/>
         </VStack>
         <FormLabel as='legend'>
           Roles you are interested in
@@ -104,8 +166,11 @@ function App() {
           <Checkbox  onChange={handleCheck} value={'swe'} colorScheme='teal' size='md'  spacing='1rem' iconSize='1rem'>
             Software Developer
           </Checkbox>
+          <Checkbox  onChange={handleCheck} value={'swe'} colorScheme='teal' size='md'  spacing='1rem' iconSize='1rem'>
+            Other
+          </Checkbox>
         </Grid>
-          <Button mt={4} colorScheme='teal' type={"submit"}>
+          <Button mt={4} colorScheme='teal' type={"submit"} onClick={(e)=>insertLead()} isDisabled={isDisabled}>
             Submit
           </Button>
       </Container>
